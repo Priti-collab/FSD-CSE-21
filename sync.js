f1c0f1c167a6641e0d5f801ebc968f49d7973092
Priti <py900115@gmail.com>
2026-08-25 10:20:57 +0530

    Add files via upload

---
+++ sync.js
@@ -0,0 +1,8 @@
+console.log("task one");
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+console.log("task two");
+console.log("task three");
+console.log("h!!!!! ABES");
+//sync execute task ansd does not jump to next task until it completed 
